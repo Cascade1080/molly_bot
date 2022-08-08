98d3064ff9631b833907abbe433f0c043f74c7dc
cascade <smccauley25@gmail.com>
2022-08-08 19:51:53 -0400

    began chatterbot

---
+++ molly.py
@@ -0,0 +1,9 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer
+
+chatbot = ChatBot("Molly")
+
+conversation = [
+    "Hello"
+]
