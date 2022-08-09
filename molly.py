6968346283e56f00bfe6e4a6b3774e116983bb75
cascade <smccauley25@gmail.com>
2022-08-08 21:44:30 -0400

    added bot functionality

--- molly.py
+++ molly.py
@@ -1,9 +1,47 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ListTrainer
 
-chatbot = ChatBot("Molly")
+chatbot = ChatBot(
+    "Molly",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    logic_adapters=[
+        "chatterbot.logic.MathematicalEvaluation",
+        "chatterbot.logic.TimeLogicAdapter"
+    ],
+    database_url="sqlite:///database.sqlite3"
+)
 
 conversation = [
-    "Hello"
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear.",
+    "Thank you.",
+    "You're welcome."
 ]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
+
+while True:
+    try:
+        conversation = [
+            "Hello",
+            "Hi there!",
+            "How are you doing?",
+            "I'm doing great.",
+            "That is good to hear.",
+            "Thank you.",
+            "You're welcome."
+        ]
+        trainer = ListTrainer(chatbot)
+        trainer.train(conversation)
+        bot_input = chatbot.get_response(input())
+        print(bot_input)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
+
+#response = chatbot.get_response("Good morning!")
+#print(response)
