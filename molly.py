c256b924a774379a576848d0cd92134f5bff8a3d
cascade <smccauley25@gmail.com>
2022-12-22 14:24:02 -0500

    added base64 encode/decode functionality

--- molly.py
+++ molly.py
@@ -1,13 +1,19 @@
 import os
+
 os.system("clear")  # clears the terminal
+os.system("cat splash.txt")  # display's 'Molly'
 
 print("Hello Steve!  What can I do for you?")
 print("1: Countdown Timer")
+print("2: Base64 Encode/Decode")
 def menu(t):  # Defines menu class
     t = input("Please select something from the above menu: ")
     if t == '1':
         import countdown
         countdown
+    elif t == '2':
+        import base64_molly
+        base64_molly
     else:
         print("That is not an option")
 
